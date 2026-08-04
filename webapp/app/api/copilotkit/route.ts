fb7f9570a2b15fc5e8b041bac6c178f99ad70e0e
Hien Nguyen <hien.nguyenvan@asnet.com.vn>
2026-08-04 23:57:11 +0000

    Update copilot api using newer version support new properties

--- webapp/app/api/copilotkit/route.ts
+++ webapp/app/api/copilotkit/route.ts
@@ -1,24 +1,14 @@
-import {
-  CopilotRuntime,
-  copilotRuntimeNextJSAppRouterEndpoint,
-} from "@copilotkit/runtime";
-import { NextRequest } from "next/server";
+import { CopilotRuntime, createCopilotRuntimeHandler } from "@copilotkit/runtime/v2";
 import { LangGraphHttpAgent } from "@copilotkit/runtime/langgraph";
 
 const runtime = new CopilotRuntime({
   agents: {
     default: new LangGraphHttpAgent({
-      url:  process.env.DEEP_RESERACH_AGENT_URL || "http://localhost:8000/copilotkit/deep_researcher",
+      url:  process.env.DEEP_RESERACH_AGENT_URL || "http://localhost:8000/copilotkit/agent/deep_researcher",
     }),
   }
 });
 
+const handler = createCopilotRuntimeHandler({ runtime, basePath: "/api/copilotkit", mode: "single-route" });
 
-export const POST = async (req: NextRequest) => {
-  const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
-    runtime,
-    endpoint: "/api/copilotkit",
-  });
-
-  return handleRequest(req);
-};
+export const POST = handler
